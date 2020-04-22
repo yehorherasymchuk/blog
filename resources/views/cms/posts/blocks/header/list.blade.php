57b03a2dc980d5fc6e7dfa582d8147dcf3a2491f
Egor Gerasimchuk <egor.igorevich.ua@gmail.com>
2020-04-22 17:34:23 +0300

    add count

--- resources/views/cms/posts/blocks/header/list.blade.php
+++ resources/views/cms/posts/blocks/header/list.blade.php
@@ -1,5 +1,5 @@
 <div class="jumbotron">
-    <h1 class="display-4">@lang('cms.posts.posts')</h1>
+    <h1 class="display-4">@lang('cms.posts.posts') ({{ \App\Models\Post::count() }})</h1>
     <hr class="my-4">
     <a class="btn btn-primary btn-lg" href="{{ route('cms.posts.create') }}" role="button">
         @lang('cms.create')
